6ca18667d67cc519f36c3e4f4b007a74abdb0244
guisfons <guilhermesfonsecaa@gmail.com>
2024-08-22 21:54:04 -0300

    changing login logo

--- functions.php
+++ functions.php
@@ -212,7 +212,7 @@ function login_logo()
     <style type="text/css">
         #login h1 a,
         .login h1 a {
-            background-image: url("<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.svg");
+            background-image: url("<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.webp");
             width: 260px;
             height: 55px;
             background-size: contain;
