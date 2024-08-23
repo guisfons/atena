6402736e1d9695eb7f4f13eb8484721b565a2bf2
guisfons <guilhermesfonsecaa@gmail.com>
2024-08-22 21:58:58 -0300

    creating page for contas de energia

---
+++ page-templates/contas-energia.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * Template Name: Contas de energia
+     * Template Post Type: page
+     *
+     * @package UAU
+     * @since 1.0.0
+     */
+
+    get_header();
+
+    the_content();
+
+    get_footer();
